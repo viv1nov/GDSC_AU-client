d2eb4d9ea3fe8494bf3bd6b9adf733cc330b4acd
Vivek <122638553+vivekbopaliya@users.noreply.github.com>
2023-09-12 16:10:22 +0530

    Update Response.jsx

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -13,14 +13,14 @@ const Response = () => {
   React.useEffect(() => {
     (async () => {
       const user = await axios.get(
-        `http://localhost:3001/users/currentUser/${localStorage.getItem(
+        `https://gdsc-au-server.onrender.com/users/currentUser/${localStorage.getItem(
           "userID"
         )}`
       );
       setCurrentUser(user.data);
     })();
     (async () => {
-      const res = await axios.get(`http://localhost:3001/question/responses`);
+      const res = await axios.get(`https://gdsc-au-server.onrender.com/question/responses`);
       setAllResponse(res.data);
     })();
   }, []);
