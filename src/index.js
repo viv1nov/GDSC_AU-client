dde131ca3e7142d47d1d5e6b0bfef1d9e51843bd
Vivek <122638553+vivekbopaliya@users.noreply.github.com>
2023-09-16 09:57:54 +0530

    Security bug fixed!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const handleContextMenu = (e) => {
+  e.preventDefault();
+  alert("Right Click is not Allowed!");
+};
+
+window.addEventListener("contextmenu", handleContextMenu);
+
+const handleKeyDown = (e) => {
+  if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) {
+    e.preventDefault();
+    alert("Developer Tools are not Allowed!");
+  }
+};
+
+window.addEventListener("keydown", handleKeyDown);
+
+const handleWindowResize = () => {
+  let isInspectOpen = false;
+
+  if (window.outerHeight - window.innerHeight > 180) {
+    isInspectOpen = true;
+  }
+  if (window.outerWidth - window.innerWidth > 180) {
+    isInspectOpen = true;
+  }
+
+  console.log(`You're not supposed to be here, pal!`);
+  isInspectOpen === false &&
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+
+  isInspectOpen === true &&
+    root.render(
+      <>
+        <h1 align="center" className="text-4xl">
+          Hello,
+          <br />
+          Please close the Developer ( Inspect ) window.
+          <br />
+          And
+          <br />
+          Refresh the website
+        </h1>
+      </>
+    );
+};
+
+window.addEventListener("resize", handleWindowResize);
+handleWindowResize();
